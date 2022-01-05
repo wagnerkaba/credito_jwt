41d6949e4d45d2f2a23cb8e4c853bf568c760965
wagnerkaba <bom.link+github@gmail.com>
2022-01-05 12:07:31 -0300

    Alteração de "home" para personalizar boas vindas com o username do usuário.

--- src/main/java/com/wagner/tqi/controller/TemplateController.java
+++ src/main/java/com/wagner/tqi/controller/TemplateController.java
@@ -1,14 +1,11 @@
 package com.wagner.tqi.controller;
 
 import com.wagner.tqi.user.ApplicationUserDetailsService;
-import io.swagger.models.Model;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.thymeleaf.context.Context;
+import org.springframework.ui.Model;
+
 
 @Controller
 @RequestMapping("/")
@@ -31,9 +28,11 @@ public class TemplateController {
 
 //  endpoint com fim de fazer testes para verificar se o usuário está autenticado no sistema
     @GetMapping("/autenticado")
-    public String getAutenticado(){
+    public String getAutenticado(Model model){
 
         String loggedUser = ApplicationUserDetailsService.getLoggedUser();
+
+        model.addAttribute("email", loggedUser);
 
         if (loggedUser!=null) return "home";
             else return "pagina_de_login";
